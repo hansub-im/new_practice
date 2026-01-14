e63c02f3548c1d00cfc0990a9348ed4872d4f07a
vosw1 <vosw1@naver.com>
2026-01-15 08:44:38 +0900

    0114 commit

---
+++ src/baekjoon/baekjoon_0115.java
@@ -0,0 +1,31 @@
+package baekjoon;
+
+import java.io.*;
+import java.nio.Buffer;
+import java.util.StringTokenizer;
+
+public class baekjoon_0115 {
+    public static void main(String[] args) throws Exception {
+        speedAB();
+    }
+
+    static void speedAB() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<T; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            bw.write((A+B) + "\n");
+
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
