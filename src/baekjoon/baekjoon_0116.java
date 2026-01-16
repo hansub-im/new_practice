c4d4f0d1e275918a0e88420b3b388c7e6c4978d8
hansub-im <sub9013@icloud.com>
2026-01-16 22:55:44 +0900

    0116 commit

---
+++ src/baekjoon/baekjoon_0116.java
@@ -0,0 +1,28 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class baekjoon_0116 {
+    public static void main(String[] args) throws Exception {
+        AB7();
+
+    }
+
+    static void AB7() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            System.out.println("Case #" + i + ": " + (A + B));
+        }
+    }
+
+}
