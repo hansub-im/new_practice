d25de744080b307c0314aacca3a5f4c6a4ebe2e0
hansub-im <sub9013@icloud.com>
2026-01-29 08:45:29 +0900

    0129 commit

--- src/baekjoon/baekjoon_0129.java
+++ src/baekjoon/baekjoon_0129.java
@@ -8,7 +8,7 @@ import java.util.StringTokenizer;
 
 public class baekjoon_0129 {
     public static void main(String[] args) throws IOException{
-        No10951();
+        No10807();
     }
     static void No10951() {
         Scanner sc = new Scanner(System.in);
@@ -20,9 +20,25 @@ public class baekjoon_0129 {
 
         }
         sc.close();
-
-
-
     }
 
+    static void No10807() {
+        Scanner sc = new Scanner(System.in);
+
+        int count = 0;
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int b = sc.nextInt();
+
+        for(int j = 0; j < arr.length; j++) {
+            if(b == arr[j]) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
 }
